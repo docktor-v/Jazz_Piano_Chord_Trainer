6491a7d879e5e0fa0bd4aa7230ae07b8700082da
docktor-v <davidWVerzwyvelt@gmail.com>
2021-12-30 20:38:08 -0500

    Added gridpane for advancing buttons

--- src/main/java/org/docktor_v/App.java
+++ src/main/java/org/docktor_v/App.java
@@ -6,10 +6,10 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -36,7 +36,6 @@ public class App extends Application {
             new Note("D", KeyCode.S, 62),
             new Note("D#", KeyCode.E, 63),
             new Note("E", KeyCode.D, 64),
-
             new Note("F", KeyCode.F, 65),
             new Note("F#", KeyCode.T, 66),
             new Note("G", KeyCode.G, 67),
@@ -48,20 +47,44 @@ public class App extends Application {
 
     );
 
-    private HBox root = new HBox(15);
-
+    private HBox keyboard = new HBox(15);
+    private VBox root = new VBox();
+    private GridPane controllerPane = new GridPane();
     private MidiChannel channel;
 
     @Override
     public void start(Stage stage) {
-        Scene scene = new Scene(createContent());
+        //controllerPane.setPrefSize(1000, 500);
+        controllerPane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
+                CornerRadii.EMPTY, new BorderWidths(5))));
+
+controllerPane.setHgap(10);
+        //controllerPane.setGridLinesVisible(true);
+        int height = 5;
+        int width = 20;
+        controllerPane.setPadding(new Insets(20,20,20,20));
+        controllerPane.add(new Button("Backward"), 0,0);
+        controllerPane.add(new Button("Reset"), 1,0);
+        controllerPane.add(new Button("Forward"), 2,0);
+//        for (int i = 0; i < width; i++) {
+//            for (int j = 0; j < height; j++) {
+//                controllerPane.add(new Button(), i, j);
+//            }
+//        }
+        controllerPane.setAlignment(Pos.CENTER);
+        root.setPrefSize(600, 500);
+        root.getChildren().add(createContent());
+        root.getChildren().add(controllerPane);
+
+
+        Scene scene = new Scene(root);
         scene.setOnKeyPressed(e -> onKeyPress(e.getCode()));
         stage.setScene(scene);
         stage.show();
     }
 
     private void onKeyPress(KeyCode key) {
-        root.getChildren()
+        keyboard.getChildren()
                 .stream()
                 .map(view -> (NoteView) view)
                 .filter(view -> view.note.key.equals(key))
@@ -94,7 +117,7 @@ public class App extends Application {
     }
 
     private void playNoteCombination(KeyCode key1, KeyCode key2) {
-        root.getChildren()
+        keyboard.getChildren()
                 .stream()
                 .map(view -> (NoteView) view)
                 .filter(view -> view.note.key.equals(key1))
@@ -116,16 +139,16 @@ public class App extends Application {
         private Note note;
         private Rectangle bg = new Rectangle(50, 200, Color.WHITE);
         private Text letter;
+
         NoteView(Note note) {
             this.note = note;
-             this.letter = new Text(note.name);
-             letter.setFill(Color.BLUE);
+            this.letter = new Text(note.name);
+
             if (note.name.contains("#")) {
                 bg.setFill(Color.BLACK);
                 bg.setHeight(180);
                 bg.setWidth(30);
                 letter.setFill(Color.WHITE);
-
             }
             bg.setStroke(Color.BLACK);
             getChildren().addAll(bg, letter);
@@ -136,22 +159,20 @@ public class App extends Application {
 
     private Parent createContent() {
         loadChannel();
-        root.setPrefSize(600, 150);
-
-        root.setSpacing(0);
-
+        keyboard.setPrefSize(600, 150);
+        keyboard.setSpacing(0);
         notes.forEach(note -> {
             NoteView view = new NoteView(note);
-            root.getChildren().add(view);
+            keyboard.getChildren().add(view);
         });
-        root.getChildren()
+        keyboard.getChildren()
                 .stream()
                 .map(view -> (NoteView) view)
                 .filter(view -> view.note.name.contains("#"))
                 .forEach(sharp -> setMargin(sharp, new Insets(0, 0, 20, 0)));
-//   root.setMargin(view, new Insets(10,20,30,40));
 
-        return root;
+
+        return keyboard;
 
     }
 
