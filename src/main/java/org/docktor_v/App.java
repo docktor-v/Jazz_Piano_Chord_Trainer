0c9e408c6a1037ea3b85080e0ebbf10f289dbc7d
docktor-v <davidWVerzwyvelt@gmail.com>
2022-01-07 16:16:53 -0500

    1

--- src/main/java/org/docktor_v/App.java
+++ src/main/java/org/docktor_v/App.java
@@ -2,12 +2,19 @@ package org.docktor_v;
 
 import javafx.animation.FillTransition;
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -35,14 +42,22 @@ import static javafx.scene.layout.HBox.setMargin;
 public class App extends Application {
     private List<Note> notes = new ArrayList<>();
 
-    int third = 108;
-    int seventh = 101;
-    int tonic = 98;
+    private int third = 108;
+    private int seventh = 101;
+    private int tonic = 98;
     private HBox keyboard = new HBox(15);
     private VBox root = new VBox();
     private GridPane controllerPane = new GridPane();
     private Button fwdButton = new Button("Forward");
+    private Button rstButton = new Button("Reset");
     private MidiChannel channel;
+    private int buttonPressCounter = 1;
+    TextArea monitor1 = new TextArea();
+    TextArea monitor2 = new TextArea();
+    TextArea monitor3 = new TextArea();
+    Image image = new Image("File:arrow.jpg");
+    ImageView pic = new ImageView(image);
+    Label chordType = new Label();
 
     @Override
     public void start(Stage stage) {
@@ -68,39 +83,52 @@ public class App extends Application {
 
         Scene scene = new Scene(root);
         List<Integer> noteList = new ArrayList<>();
-        scene.setOnKeyPressed(e -> {
-            ArrayList<Note> playedNotes = notes.stream().filter(s -> s.key.equals(e.getCode())).collect(Collectors.toCollection(ArrayList::new));
-            playedNotes.forEach(s -> playKey(s.number));
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                ArrayList<Note> playedNotes = notes.stream().filter(s -> s.key.equals(keyEvent.getCode())).collect(Collectors.toCollection(ArrayList::new));
+                playedNotes.forEach(s -> playKey(s.number));
+            }
         });
-
-        //.findFirst().ifPresent(App:test())));
-        //orElse(null).number));
-
+        rstButton.setOnAction(((event) -> {
+            buttonPressCounter = 1;
+            third = 101; //D
+            seventh = 108; //C
+            tonic = 98; //F
+        }));
 
         fwdButton.setOnAction((event)
                 -> {
-            moveTrainerForward(third, seventh, tonic);
-            third--;
-            seventh--;
-        });
+            if (buttonPressCounter == 1) {
+                monitor1.appendText("Play minor seventh, then move finger #five down one semitone");
+                chordType.setText("Minor Seventh");
+                playNoteCombination(third, seventh, tonic);
+                seventh--;
+                tonic = tonic - 7;
+                buttonPressCounter++;
+            } else if (buttonPressCounter == 2) {
+                monitor2.appendText("Dominant seventh");
+                playNoteCombination(third, seventh, tonic);
+                third--;
+                tonic = tonic + 5;
+                buttonPressCounter++;
+            } else if (buttonPressCounter == 3) {
+                monitor3.appendText("Major seventh");
+                playNoteCombination(third, seventh, tonic);
+                buttonPressCounter++;
 
+            } else if (buttonPressCounter % 4 == 0) {
+                // monitor3.appendText("Major seventh");
+                playNoteCombination(third, seventh, tonic);
+                buttonPressCounter = 1;
+                //do nothing.
+            }
+        });
         stage.setScene(scene);
         stage.show();
     }
 
-    public void test() {
-
-    }
-
-    public void moveTrainerForward(int third, int seventh, int root) {
-        playNoteCombination(third, seventh, root);
-        playKey(98);
-
-    }
-
-
     public void playKey(int key) {
-
         keyboard.getChildren()
                 .stream()
                 .map(view -> (NoteView) view)
@@ -129,10 +157,7 @@ public class App extends Application {
                         ft.setCycleCount(2);
                         ft.setAutoReverse(true);
                         ft.play();
-
-
                         channel.noteOn(view.note.number, 90);
-
                     }
                 });
     }
@@ -145,7 +170,7 @@ public class App extends Application {
                 .forEach(view -> {
                     if (!view.note.name.contains("#")) {
                         FillTransition ft = new FillTransition(
-                                Duration.seconds(.15),
+                                Duration.seconds(.75),
                                 view.bg,
                                 Color.WHITE,
                                 Color.BLACK
@@ -158,7 +183,7 @@ public class App extends Application {
                         view.bg.setFill(Color.BLACK);
                     } else {
                         FillTransition ft = new FillTransition(
-                                Duration.seconds(.15),
+                                Duration.seconds(.75),
                                 view.bg,
                                 Color.BLACK,
                                 Color.WHITE
@@ -166,14 +191,9 @@ public class App extends Application {
                         ft.setCycleCount(2);
                         ft.setAutoReverse(true);
                         ft.play();
-
-
                         channel.noteOn(view.note.number, 90);
-
                     }
-
                 });
-
     }
 
     private static class NoteView extends StackPane {
@@ -200,22 +220,56 @@ public class App extends Application {
     }
 
     private Parent createControlContent() {
+
         controllerPane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
                 CornerRadii.EMPTY, new BorderWidths(5))));
-        controllerPane.setHgap(10);
-        controllerPane.setMinSize(400, 200);
-        controllerPane.setPadding(new Insets(20, 20, 20, 20));
-        controllerPane.add(new Button("Reset"), 1, 0);
-        controllerPane.add(fwdButton, 2, 0);
+        for (int i = 0; i < 9; i++) {
+            ColumnConstraints column = new ColumnConstraints(100);
+            controllerPane.getColumnConstraints().add(column);
+        }
+        for (int i = 0; i < 8; i++) {
+            RowConstraints row = new RowConstraints(35);
+            controllerPane.getRowConstraints().add(row);
+        }
+        controllerPane.setHgap(5);
+        controllerPane.setMinSize(400, 350);
+        controllerPane.setVgap(5);
+        controllerPane.setHgap(5);
+        monitor1.setWrapText(true);
+        monitor2.setWrapText(true);
+        monitor3.setWrapText(true);
+        GridPane.setRowSpan(monitor1, 2);
+        GridPane.setColumnSpan(monitor1, 3);
+        GridPane.setRowSpan(monitor2, 2);
+        GridPane.setColumnSpan(monitor2, 3);
+        GridPane.setRowSpan(monitor3, 2);
+        GridPane.setColumnSpan(monitor3, 3);
+        monitor1.setEditable(false);
+
+        chordType.setText("Chord types will show here");
+
+        controllerPane.setGridLinesVisible(true);
+
+        //i is x axis i1 is Y axis  page.add(Node, colIndex, rowIndex, colSpan, rowSpan):
+        controllerPane.add(rstButton, 0, 0);
+        controllerPane.add(fwdButton, 1, 0);
+        controllerPane.add(chordType, 2, 0);
+
+        controllerPane.add(monitor1, 6, 0);
+        controllerPane.add(monitor2, 6, 3);
+        controllerPane.add(monitor3, 6, 6);
+
+        pic.setFitHeight(30);
+        pic.setFitWidth(50);
+        controllerPane.add(pic, 5, 0);
+        GridPane.setHalignment(pic, HPos.RIGHT);
+        pic.setVisible(false);
         controllerPane.setAlignment(Pos.CENTER);
         return controllerPane;
-
     }
 
     private Parent createKeyboardContent() {
         loadChannel();
-        //   keyboard.setPrefSize(600, 150);
-        HBox.setHgrow(keyboard, Priority.ALWAYS);
 
         keyboard.setSpacing(0);
         notes.forEach(note -> {
